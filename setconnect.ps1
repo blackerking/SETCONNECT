0f4ea0fb1dfc5f8196ba4ba084c1bf3e8c87dbc2
blackerking <blackerking@googlemail.com>
2019-04-09 13:14:33 +0200

    Update setconnect.ps1

--- setconnect.ps1
+++ setconnect.ps1
@@ -1,5 +1,9 @@
-﻿##Hole Anzahl der Verbindungen##
-$Anzahl = Get-NetConnectionProfile |select InterfaceIndex
+##Hole Interface-Nummr der Verbindungen##
+[String]$Anzahl = Get-NetConnectionProfile |select InterfaceIndex
+
+##String bereinigen##
+    $Anzahl = $Anzahl.Remove(0,17)
+    $Anzahl = $Anzahl.Replace("}",$null)
 
 ##Setze alle Verbindungen##
 Set-NetConnectionProfile -InterfaceIndex $Anzahl -NetworkCategory DomainAuthenticated
